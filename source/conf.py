3786192c5d14a79edd77c50039587b46116ddc29
Louis Nguyen <45759180+louisnguyen12@users.noreply.github.com>
2019-06-17 15:18:08 +0700

    added favicon.ico

--- source/conf.py
+++ source/conf.py
@@ -143,7 +143,7 @@ html_theme = 'sphinx_rtd_theme'
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
-
+html_favicon = './img/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
